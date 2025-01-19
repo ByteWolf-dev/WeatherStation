7a05f16fec421d6f6e171bbf9d12aced0472cce0
Daniel Fischlmayr <d.fischlmayr@students.htl-leonding.ac.at>
2025-01-19 20:07:49 +0100

    works on emulator but parsing problem

--- weatherstationandroid/app/src/main/java/com/example/weatherstation_tadeot/model/RetrofitClient.kt
+++ weatherstationandroid/app/src/main/java/com/example/weatherstation_tadeot/model/RetrofitClient.kt
@@ -4,17 +4,15 @@ import com.google.gson.GsonBuilder
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonReader
 import com.squareup.moshi.JsonWriter
-import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.format.DateTimeFormatter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.137.1:8080"
+    private const val BASE_URL = "http://10.0.2.2:8080"
 
 //    private val moshi = Moshi.Builder()
 //        .add(LocalDateTimeAdapter())
